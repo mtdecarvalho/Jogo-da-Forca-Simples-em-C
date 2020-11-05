9139d96d16e59a3eb0141e07f23931513e504b6a
von Carvalho <67925033+mtdecarvalho@users.noreply.github.com>
2020-11-05 15:03:15 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,3 @@
-//10- Faça o jogo da Forca. 
-//    inicialmente o programa deve ler um frase secreta, limpar a tela e iniciar o jogo. 
-//    Na tela devem aparacer: As letras chutas, a Frase secreta com _ nas letras nao chutadas, e a qtd de vida, que inicialmente é 5.
-//    apos 5 erros o jogador perde.
-//    obs: o usuario so vai chutar letras de A-Z, entao se na frase tivermos qualquer caracter diferente este ja aparece revelado.
-//         e a Forca nao faz distinção de maiuscula e minuscula
-
 #include <stdio.h>
 
 #include "forca.h"
